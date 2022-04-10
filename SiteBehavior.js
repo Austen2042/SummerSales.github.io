bd42bcdbc6b8ac16f1ba0b9c97b31525e37fe6ec
Austen King <austen2042@gmail.com>
2022-04-10 12:17:50 -0500

    Added penny list content

--- SiteBehavior.js
+++ SiteBehavior.js
@@ -1,2 +1,13 @@
+var PennyList = document.getElementById("pItems");
 
+setInterval(ChangeButtonColorYellow, 1000);
+setInterval(ChangeButtonColorWhite, 2000);
+
+function ChangeButtonColorYellow(){
+PennyList.style.backgroundColor="yellow";
+}
+
+function ChangeButtonColorWhite(){
+    PennyList.style.backgroundColor="white";
+}
 
