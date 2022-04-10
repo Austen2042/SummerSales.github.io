b51ccd6654bd37f6411eba15d0da22dcf4425cc5
Austen King <austen2042@gmail.com>
2022-04-10 18:28:46 -0500

    Added CSS to ticker color and backgrond attachment

--- SiteBehavior.js
+++ SiteBehavior.js
@@ -1,13 +1,18 @@
 var PennyList = document.getElementById("pItems");
+var ticker = document.getElementById("Ticker");
 
-setInterval(ChangeButtonColorYellow, 1000);
-setInterval(ChangeButtonColorWhite, 2000);
+
+setTimeout(ChangeButtonColorYellow, 2000);
 
 function ChangeButtonColorYellow(){
 PennyList.style.backgroundColor="yellow";
+ticker.style.color="yellow";
+setTimeout(ChangeButtonColorWhite, 2000);
 }
 
 function ChangeButtonColorWhite(){
     PennyList.style.backgroundColor="white";
+    ticker.style.color="white";
+    setTimeout(ChangeButtonColorYellow, 2000);
 }
 
