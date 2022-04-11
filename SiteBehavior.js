99778dd428db95c2533c6ef21ca67896c61f1c34
Austen King <austen2042@gmail.com>
2022-04-11 14:32:17 -0500

    chand=ged index color

--- SiteBehavior.js
+++ SiteBehavior.js
@@ -1,18 +1,15 @@
 var PennyList = document.getElementById("pItems");
-var ticker = document.getElementById("Ticker");
 
 
 setTimeout(ChangeButtonColorYellow, 2000);
 
 function ChangeButtonColorYellow(){
 PennyList.style.backgroundColor="yellow";
-ticker.style.color="yellow";
 setTimeout(ChangeButtonColorWhite, 2000);
 }
 
 function ChangeButtonColorWhite(){
     PennyList.style.backgroundColor="white";
-    ticker.style.color="white";
     setTimeout(ChangeButtonColorYellow, 2000);
 }
 
